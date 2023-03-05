45517c73a3390769dda6def5921515e535da64e6
Lex Benjamin <abessilfie@st.ug.edu.gh>
2023-03-05 18:56:12 -0500

    Backend work on the Login Page

--- client/src/pages/AdmissionPage.jsx
+++ client/src/pages/AdmissionPage.jsx
@@ -5,6 +5,8 @@ import '../styles/AdmissionPage.css'
 import sts from '../assets/sts.png'
 
 const AdmissionPage = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   const [loading, setLoading] = useState(false)
 
   return (
@@ -25,7 +27,7 @@ const AdmissionPage = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
               </svg>
 
-              <input type="text" placeholder='SERIAL PIN' />
+              <input type="email" placeholder='Email Address' value={email} onChange={e => setEmail(e.target.value)} />
             </div>
 
             {/* Student PIN Input */}
@@ -37,7 +39,7 @@ const AdmissionPage = () => {
               </svg>
 
 
-              <input type="password" placeholder='PASSCODE' />
+              <input type="password" placeholder='Password' value={password} onChange={e => setPassword(e.target.value)}/>
             </div>
 
             {/* Button */}
