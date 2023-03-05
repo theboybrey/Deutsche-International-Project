18001447efec11d0bf067f5c43092e71c6c19866
Lex Benjamin <abessilfie@st.ug.edu.gh>
2023-03-05 18:37:59 -0500

    Login Page Desktop View completed

--- client/src/pages/AdmissionPage.jsx
+++ client/src/pages/AdmissionPage.jsx
@@ -5,7 +5,7 @@ import '../styles/AdmissionPage.css'
 import sts from '../assets/sts.png'
 
 const AdmissionPage = () => {
-  const[loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   return (
     <div className='-ml-8 grow flex items-center justify-around'>
@@ -33,20 +33,23 @@ const AdmissionPage = () => {
             <div className="passcode-part flex gap-2">
 
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z" />
               </svg>
+
 
               <input type="password" placeholder='PASSCODE' />
             </div>
 
             {/* Button */}
-            <button className="primary-btn" >
-              {loading ? 'Loading...' : 'Login'}
+            <div className="btn-container">
+              <button className="primary-btn" >
+                {loading ? 'Loading...' : 'Login'}
 
-              {
-                loading && <span className="spinner-border border-green-600 spinner-border-sm" role="status" aria-hidden="true"></span>
-              }
-            </button>
+                {
+                  loading && <span className="spinner-border border-gray-500 spinner-border-sm" role="status" aria-hidden="true"></span>
+                }
+              </button>
+            </div>
 
           </form>
         </div>
