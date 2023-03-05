89f37cf83fb0c7e2c3456217b6f024ee6533151d
Lex Benjamin <abessilfie@st.ug.edu.gh>
2023-03-05 09:42:00 -0500

    Styling Mobile UI for the App

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -110,7 +110,7 @@ const LandingPage = () => {
       </div>
 
       {/* Read More Button */}
-      <div className="btn justify-center ">
+      <div className="btn justify-center readMoreBtn">
         <Link className="bg-black text-white py-3 px-5 flex gap-2">Read More
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
