1ef95b41c141774cf89d8af6b2e74c88dcd7c16a
Lex Benjamin <abessilfie@st.ug.edu.gh>
2023-03-22 19:02:50 -0400

    Debugging the Login Page

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import '../styles/AdmissionPage.css'
-import { Link } from 'react-router-dom'
-import { useStateValue } from '../context/StateProvider'
+import { useState } from 'react'
+import '../styles/AdmissionPage.css';
+import { Link } from 'react-router-dom';
+import { useStateValue } from '../context/stateProvider'
 import { LOGIN } from '../utils/Auth'
-import loginBg from '../assets/loginBg.jpg'
+import '../styles/LoginPage.css'
 
 // Importing the Logo
 import sts from '../assets/sts.png'
@@ -37,21 +37,18 @@ const LoginPage = () => {
 
 
   return (
-    <div className='-ml-8 grow flex items-center justify-around'
-     /** style={{
-        backgroundImage: `url(${loginBg})`,
-      }}**/>
+    <div className='-ml-8 grow flex items-center justify-around'>
       <div className="admission-section mb-64 flex">
         {/* Login Option */}
         <div className="user-login-section">
           <div className="login-logo mb-4">
-            <img src={sts} alt="STS Logo" width={250} />
+            <img src={sts} alt="STS Logo" width={200} />
           </div>
 
-          <form action="" className='max-w-md me-auto'>
+          <form action="" className='max-w-md me-auto' >
             {/* Username Input */}
             <div className="username-part flex gap-2">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="w-6 h-6 email-icon">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
               </svg>
 
@@ -63,7 +60,7 @@ const LoginPage = () => {
 
             <div className="passcode-part flex gap-2">
 
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="w-6 h-6 password-icon">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z" />
               </svg>
 
@@ -84,9 +81,14 @@ const LoginPage = () => {
             </div>
 
           </form>
+
+          {/* Sign Up Link */}
+          <div className="text-center py-2 text-gray-500">
+            <p>Dont have an account yet?<Link to="/enroll" className="text-gray-500 underline hover:text-white">Join Us Now</Link>
+            </p>
+          </div>
         </div>
       </div>
-
     </div>
   )
 }
--- client/src/pages/AdmissionPage.jsx
+++ client/src/pages/AdmissionPage.jsx
@@ -1,21 +1,24 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 import { SIGNUP } from '../utils/Auth';
 
+import sts from '../assets/sts.png'
+
+
 const AdmissionPage = () => {
-  const[firstName, setFirstname] = useState('');
-  const[lastName, setLastname] = useState('');
-  const[email, setEmail] = useState('');
-  const[password, setPassword] = useState('');
-  const[gender, setGender] = useState('');
-  const[address, setAddress] = useState('');
-  const[date_of_birth, setDateOfBirth] = useState('');
-  const[telephone, setTelephone] = useState('');
-  const[loading, setLoading] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [gender, setGender] = useState('');
+  const [address, setAddress] = useState('');
+  const [date_of_birth, setDateOfBirth] = useState('');
+  const [telephone, setTelephone] = useState('');
+  const [loading, setLoading] = useState('');
 
   // Register A Student
-  const registerUserv = (e) => {
+  const registerUser = (e) => {
     e.preventDefault();
     setLoading(true);
     const userData = {
@@ -37,9 +40,66 @@ const AdmissionPage = () => {
     })
   }
   return (
-    <div>
-      
+    <div className='mt-4 grow flex items-center justify-around'>
+      <div className="mb-60 sign-up-pst">
+        <img src={sts} width={200}></img>
+        <h1 className='text-white text-3xl -mb-2 hidden'>Create a new account</h1>
+        <form action="" className='max-w-md mx-auto' onSubmit={registerUser}>
 
+          {/* Name Input Field */}
+          <div className="flex gap-2 name-field">
+            {/* Fisrt Name */}
+            <input type="text" placeholder="First Name"
+              value={firstName}
+              onChange={e => setFirstName(e.target.value)} />
+
+            {/* Last Name Input */}
+            <input type="text" placeholder="Last Name"
+              value={lastName} onChange={e => setLastName(e.target.value)} />
+          </div>
+
+          {/* Email Input */}
+          <input type="email" placeholder="username@domain.com"
+            value={email} onChange={e => setEmail(e.target.value)} />
+
+          {/* Date of Birth */}
+          <input type="date" value={date_of_birth} onChange={e => setDateOfBirth(e.target.value)} style={{
+            color: 'gray'
+          }}/>
+
+          {/*Gender Input */}
+          <input type="text" placeholder="Gender"
+            value={gender} onChange={e => setGender(e.target.value)} />
+
+          {/* Address Input */}
+          <input type="text" placeholder="Residential Address"
+            value={address} onChange={e => setAddress(e.target.value)} />
+
+
+          {/* Telephone Input */}
+          <input type="text" placeholder="+233 (0)"
+            value={telephone} onChange={e => setTelephone(e.target.value)} />
+
+
+          {/* Password Input */}
+          <input type="password" placeholder="Enter Password"
+            value={password} onChange={e => setPassword(e.target.value)} />
+
+          {/* Button */}
+          <button className="primary signup-btn">
+            {loading ? "Loading..." : "Register"}
+            {
+              loading && <span className="spinner-border border-white-500 spinner-border-sm" role="status" aria-hidden="true"></span>
+            }
+          </button>
+        </form>
+
+        {/* Sign Up Link */}
+        <div className="text-center py-2 text-gray-500 -mt-6">
+          <p>Already have an account?            <Link to="/login" className="text-white underline">Login</Link>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
